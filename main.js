03bdb720e124b00a4f834129821ce96321a4394f
Kevin Tran <ktran050@gmail.com>
2020-03-24 16:36:16 -0700

    successfully made a fetch call on a static user

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-let header = new Headers();
+const myHeaders = new Headers({
+  Accept: "application/vnd.github.v3+json"
+});
+
+let options = {
+  method: "GET",
+  header: myHeaders,
+  redirect: "follow"
+};
 
 let fetchUrl = "https://api.github.com/users/ktran050/repos";
 
 // fetches all repositories when given a username
 function fetchRepos(username) {
-  fetch(fetchUrl).then(function(result) {
-    console.log(result);
-  });
+  fetch(fetchUrl, options)
+    .then(function(result) {
+      if (!result.ok) {
+        throw new Error("result not ok");
+      }
+      return result.text();
+    })
+    .then(function(result) {
+      console.log(result);
+    })
+    .catch(function(error) {
+      console.log("error", error);
+    });
 }
 
 // Reads user input on form submit and calls fetch repo to handle the fetching
